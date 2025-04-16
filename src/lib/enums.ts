eb119a039100a1e641af56f52ed1b809dc78ac66
Abdullah Al Zarif <zarif_al96@outlook.com>
2025-04-16 14:40:22 +0600

    feat: create necessary enums

---
+++ src/lib/enums.ts
@@ -0,0 +1,31 @@
+export enum CurrencyType {
+	USD = "USD",
+	GBP = "GBP",
+	BDT = "BDT",
+}
+
+export enum EmployeeStatus {
+	ACTIVE = "active",
+	INACTIVE = "inactive",
+	TERMINATED = "terminated",
+	ON_LEAVE = "on_leave",
+	PROBATION = "probation",
+}
+
+export enum JobOpeningStatus {
+	OPEN = "open",
+	CLOSED = "closed",
+}
+
+export enum LeaveRequestStatus {
+	PENDING = "pending",
+	APPROVED = "approved",
+	REJECTED = "rejected",
+}
+
+export enum LeaveType {
+	VACATION = "vacation",
+	SICK_LEAVE = "sick_leave",
+	PARENTAL_LEAVE = "parental_leave",
+	MEDICAL_LEAVE = "medical_leave",
+}
