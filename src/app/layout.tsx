e10137ddcf3f5b6005ee73b7fd007a6db767ace3
Abdullah Al Zarif <zarif_al96@outlook.com>
2025-04-16 16:57:20 +0600

    build: add sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -33,6 +34,8 @@ export default function RootLayout({
 					<AppSidebar />
 					<SidebarInset>{children}</SidebarInset>
 				</SidebarProvider>
+
+				<Toaster className="pointer-events-auto" richColors />
 			</body>
 		</html>
 	);
