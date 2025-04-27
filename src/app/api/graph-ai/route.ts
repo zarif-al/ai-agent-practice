667cb1dad4c447003cbbad298a46bff38b03e139
Abdullah Al Zarif <zarif_al96@outlook.com>
2025-04-27 22:06:27 +0600

    refactor: use JSON represenation of table for context

--- src/app/api/graph-ai/route.ts
+++ src/app/api/graph-ai/route.ts
@@ -1,7 +1,7 @@
 import { appendResponseMessages, streamText } from 'ai';
 import { saveChat } from '@/lib/chat-store';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
-import { getSchemaAsString } from './context/database';
+import tablesJSON from '@/db/schema/tables.json';
 import { z } from 'zod';
 
 const requestBodySchema = z.object({
@@ -44,7 +44,7 @@ export async function POST(req: Request) {
       model: openrouter.chat('google/gemini-2.0-flash-exp:free'),
       system:
         `Here is the database schema of the system you are working with:
-         Schema: ${getSchemaAsString()}
+         Schema: ${JSON.stringify(tablesJSON, null, 2)}
         ` +
         `Your job is to help answer any and all queries regarding this database` +
         `If the user wants to generate graphs based on data from this database you will first validate the users request against the schema to see if it is a valid request.`,
