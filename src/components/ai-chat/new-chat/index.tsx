207ea6b836dfe1759d407a28a5e17a49705319ed
Abdullah Al Zarif <zarif_al96@outlook.com>
2025-05-09 20:15:13 +0600

    refactor: update import

--- src/components/ai-chat/new-chat/index.tsx
+++ src/components/ai-chat/new-chat/index.tsx
@@ -9,7 +9,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import type { IChat } from '../interface';
 import { useQueryClient } from '@tanstack/react-query';
-import { createChat } from '@/app/api/graph-ai/utils/chat-store';
+import { createChat } from '@/utils/chat-store';
 
 interface NewChatModalProps {
   isOpen: boolean;
